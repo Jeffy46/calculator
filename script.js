99d2585f9c7b45a092b5629d2ddd1e7364be47ff
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2025-01-06 17:42:58 -0500

    darken buttons when clicked

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let container = document.querySelector(".display");
+let container = document.querySelector('.display');
 let load = () => {
-    // Your code here
+    let temp = document.querySelectorAll('.button');
+    for(let i = 0;i < temp.length; i++){
+        temp[i].addEventListener('mousedown', ()=>temp[i].style.filter='brightness(80%)');
+        temp[i].addEventListener('mouseup', ()=>temp[i].style.filter='brightness(100%)');
+    }
 };
