223bfa2f6032ca84c54cebebaf6c247b19f445b3
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2025-01-08 19:05:48 -0500

    everything excluding operations and equal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let display = document.querySelector('.display');
+let num = "";
+let altNum = "";
 let operation = "";
 //adds functions to buttons
 let load = () => {
@@ -11,9 +13,8 @@ let load = () => {
     //appends numbers to user display when clicked
     let temp2 = document.querySelectorAll("#num");
         for(let i = 0; i < temp2.length; i++){
-        console.log(temp2[i]);
         temp2[i].addEventListener('click', ()=>{
-            console.log(temp2[i]);
+            num+=temp2[i].textContent;
             display.append(temp2[i].textContent);
         })
     }
@@ -21,5 +22,32 @@ let load = () => {
     let temp3 = document.querySelector("#AC");
     temp3.addEventListener('click', ()=>{
         display.textContent = ""
+        num="";
     })
-};
+    //postive negative toggle
+    let temp4 = document.querySelector("#posNeg");
+    temp4.addEventListener('click', () =>{
+        num*=-1;
+        refresh();
+    });
+    //deletes last digit
+    let temp5 = document.querySelector("#delete");
+        temp5.addEventListener('click', ()=>{
+            try{
+                num = num.substring(0,num.length-1);
+                refresh();
+            }catch(err){
+            }
+        })
+    }
+    
+    //divide by hundred
+    let temp6 = document.querySelector("#divByHundred");
+    temp6.addEventListener('click', ()=>{
+        num = parseFloat(parseFloat(num/100).toFixed(100));
+        refresh();
+    })
+
+let refresh = () =>{
+    display.innerHTML=num;
+}
